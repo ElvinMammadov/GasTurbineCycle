10f979c633bfe97f43c9b02af3c99d2675ecc361
Elvin <elvin.m@hotmail.com>
2021-09-20 15:15:51 +0200

    Choose Jet models

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,7 @@ class _HomeState extends State<Home> {
   int dropdownValue = 1;
   bool isVisible = false;
   int items = 1;
+  String dropdownValueJet = 'Turbojet';
 
   @override
   Widget build(BuildContext context) {
@@ -24,165 +25,235 @@ class _HomeState extends State<Home> {
         title: Text('Gas Turbine Cycle'),
         centerTitle: true,
       ),
-      body: Container(
-        //height: 500,
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(
-              height: 20.0,
-            ),
-            const Text(
-              "How many values has to be added ?",
-              style: TextStyle(
-                fontSize: 20,
+      body: SingleChildScrollView(
+        child: Container(
+          //height: 500,
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 20.0,
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              //mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                DropdownButton<int>(
-                  value: dropdownValue,
-                  icon: const Icon(Icons.arrow_downward),
-                  iconSize: 24,
-                  elevation: 5,
-                  style: TextStyle(
-                    color: Colors.blue[400],
-                    fontSize: 20,
-                  ),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.blue[400],
-                  ),
-                  onChanged: (int newValue) {
-                    setState(() {
-                      dropdownValue = newValue;
-                      //print('value is $dropdownValue');
-                    });
-                  },
-                  items: <int>[1, 2, 3, 4, 5, 6, 7, 8]
-                      .map<DropdownMenuItem<int>>((int value) {
-                    return DropdownMenuItem<int>(
-                      value: value,
-                      child: Text(
-                        value.toString(),
-                      ),
-                    );
-                  }).toList(),
+              const Text(
+                "Choose one of the jets",
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-                ButtonTheme(
-                  buttonColor: Colors.blue[400],
-                  minWidth: 50.0,
-                  height: 30.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: RaisedButton(
-                    onPressed: () {
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                //mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  DropdownButton<String>(
+                    value: dropdownValueJet,
+                    icon: const Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 5,
+                    style: TextStyle(
+                      color: Colors.blue[400],
+                      fontSize: 20,
+                    ),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.blue[400],
+                    ),
+                    onChanged: (String newValue) {
                       setState(() {
-                        isVisible = true;
-                        items = dropdownValue;
+                        dropdownValueJet = newValue;
+                        //print('value is $dropdownValue');
                       });
-                      //Navigator.pushNamed(context, '/values');
                     },
-                    child: Text(
-                      'Choose',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 17,
+                    items: <String>['Turbojet', 'Turbofan','Ramjet']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  ButtonTheme(
+                    buttonColor: Colors.blue[400],
+                    minWidth: 50.0,
+                    height: 30.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          // isVisible = true;
+                          // items = dropdownValue;
+                        });
+                        //Navigator.pushNamed(context, '/values');
+                      },
+                      child: Text(
+                        'Choose',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+              const Text(
+                "Choose combination of input parameters",
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Visibility(
-              visible: isVisible,
-              maintainSize: true,
-              maintainAnimation: true,
-              maintainState: true,
-              child: Container(
-                // height: 300.0,
-                // width: 350.0,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height:
-                          items == 8 ? screenHeight * 0.5 : screenHeight * 0.3,
-                      width: screenWidth * 0.9,
-                      child: GridView.count(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 2,
-                        padding: const EdgeInsets.all(10.0),
-                        children: List.generate(items, (index) {
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Item ${index + 1}',
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                //mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  DropdownButton<int>(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 5,
+                    style: TextStyle(
+                      color: Colors.blue[400],
+                      fontSize: 20,
+                    ),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.blue[400],
+                    ),
+                    onChanged: (int newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                        //print('value is $dropdownValue');
+                      });
+                    },
+                    items: <int>[1, 2, 3, 4, 5, 6, 7, 8]
+                        .map<DropdownMenuItem<int>>((int value) {
+                      return DropdownMenuItem<int>(
+                        value: value,
+                        child: Text(
+                          value.toString(),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  ButtonTheme(
+                    buttonColor: Colors.blue[400],
+                    minWidth: 50.0,
+                    height: 30.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          isVisible = true;
+                          items = dropdownValue;
+                        });
+                        //Navigator.pushNamed(context, '/values');
+                      },
+                      child: Text(
+                        'Choose',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Visibility(
+                visible: isVisible,
+                maintainSize: true,
+                maintainAnimation: true,
+                maintainState: true,
+                child: Container(
+                  // height: 300.0,
+                  // width: 350.0,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height:
+                            items == 8 ? screenHeight * 0.5 : screenHeight * 0.3,
+                        width: screenWidth * 0.9,
+                        child: GridView.count(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 2,
+                          padding: const EdgeInsets.all(10.0),
+                          children: List.generate(items, (index) {
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Item ${index + 1}',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Add value ${index + 1}",
+                                  ),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ],
+                            );
+                          }),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 50.0,
+                          ),
+                          ButtonTheme(
+                            buttonColor: Colors.blue[400],
+                            minWidth: 50.0,
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: RaisedButton(
+                              onPressed: () {
+                                // setState(() {
+                                //   isVisible = true;
+                                //   items = dropdownValue;
+                                // });
+                                //Navigator.pushNamed(context, '/values');
+                              },
+                              child: Text(
+                                'Add',
                                 style: TextStyle(
-                                  color: Colors.black,
+                                  color: Colors.white,
                                   fontSize: 17,
                                 ),
                               ),
-                              TextField(
-                                decoration: InputDecoration(
-                                  hintText: "Add value ${index + 1}",
-                                ),
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ],
-                          );
-                        }),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        SizedBox(
-                          width: 50.0,
-                        ),
-                        ButtonTheme(
-                          buttonColor: Colors.blue[400],
-                          minWidth: 50.0,
-                          height: 30.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: RaisedButton(
-                            onPressed: () {
-                              // setState(() {
-                              //   isVisible = true;
-                              //   items = dropdownValue;
-                              // });
-                              //Navigator.pushNamed(context, '/values');
-                            },
-                            child: Text(
-                              'Add',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
